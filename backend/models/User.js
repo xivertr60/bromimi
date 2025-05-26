1ce84c0cded2cc2f46c67c273a7deff8df203345
shop <pepigames@outlook.es>
2025-05-26 19:42:47 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,8 @@
-
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  verified: { type: Boolean, default: false },
+  verificationCode: { type: String },
+});
+module.exports = mongoose.model('User', UserSchema);
